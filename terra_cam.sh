bf55f0581a0d796b872b95316f0bfbe3d35f43fb
kaju666 <kaju666@gmail.com>
2020-06-18 21:12:41 +0200

    Update terra_cam.sh

--- terra_cam.sh
+++ terra_cam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/budi
 
 DATETIME=$(date +"%e.%m.%y %R")
-TERRADATA=$(python terra_api.py)
+TERRADATA=$(python3 terra_api.py)
 
 #Get image from webcam or copy RAW image from TerrariumPI
 #raspistill -rot 90 -o cam.jpg -w 1944 -h 2592 -q 100 -x
@@ -22,3 +23,8 @@ convert cam_overlay.jpg \
 #Upload to ftp
 sleep 2
 curl -T cam_final.jpg ftp://hostname//terrarium.jpg --user username:password
+
+#Upload to www
+#sleep 2
+#curl -F "uploaded_file=@terrarium.jpg" http://hostname.com/upload.php
+
