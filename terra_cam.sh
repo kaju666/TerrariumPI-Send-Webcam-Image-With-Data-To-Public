7801519670b5614d3a53377613467857b41ac4b8
kaju666 <kaju666@gmail.com>
2021-08-18 21:34:03 +0200

    Update terra_cam.sh

--- terra_cam.sh
+++ terra_cam.sh
@@ -6,7 +6,7 @@ TERRADATA=$(python3 terra_api.py)
 
 #Get image from webcam or copy RAW image from TerrariumPI
 #raspistill -rot 90 -o cam.jpg -w 1944 -h 2592 -q 100 -x
-cp /home/pi/TerrariumPI/webcam/799bd9a9c5fbfb43fb382ec550b4b943/799bf4d9c5fbfbb4fbe82ec550b4b943_raw.jpg cam.jpg
+cp /home/pi/TerrariumPI/webcam/91b9918af1fc05b955799d4bd25c381c/91b9918af1fc05b955799d4bd25c381c_raw.jpg cam.jpg
 
 #Add overlay
 composite -gravity center overlay.png cam.jpg cam_overlay.jpg
@@ -16,15 +16,11 @@ convert cam_overlay.jpg \
 	-pointsize 40 -fill white -annotate +1510+50  \
 	"Terrarium PI - Thea " \
 	-pointsize 40 -fill white -annotate +1510+90  \
-	"Date: $DATETIME" \
+	"Taken: $DATETIME" \
 	-pointsize 40 -fill white -annotate +1510+130  \
 	"$TERRADATA" \
-	cam_final.jpg
+	terrarium.jpg
 #Upload to ftp
 sleep 2
-curl -T cam_final.jpg ftp://hostname//terrarium.jpg --user username:password
-
-#Upload to www
-#sleep 2
-#curl -F "uploaded_file=@terrarium.jpg" http://hostname.com/upload.php
+curl -F "uploaded_file=@terrarium.jpg" https://xxx.com/upload.php
 
